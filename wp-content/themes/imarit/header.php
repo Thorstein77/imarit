ef9ad32a510f25cfafc63a2f1c8d1a9e75af64fd
Thorstein77 <jubbelidiot@gmail.com>
2019-11-08 10:03:29 +0100

    underscore theme

--- wp-content/themes/imarit/header.php
+++ wp-content/themes/imarit/header.php
@@ -25,7 +25,24 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'imarit' ); ?></a>
 
 	<header id="masthead" class="site-header">
-        
+        <div class="site-branding">
+            <?php
+            the_custom_logo();
+            if ( is_front_page() && is_home() ) :
+                ?>
+                <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                <?php
+            else :
+                ?>
+                <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+                <?php
+            endif;
+            $imarit_description = get_bloginfo( 'description', 'display' );
+            if ( $imarit_description || is_customize_preview() ) :
+                ?>
+                <p class="site-description"><?php echo $imarit_description; /* WPCS: xss ok. */ ?></p>
+            <?php endif; ?>
+        </div><!-- .site-branding -->
 
         <nav id="site-navigation" class="main-navigation">
             <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'imarit' ); ?></button>
@@ -38,7 +55,7 @@
         </nav><!-- #site-navigation -->
 
         <div>
-
+            <img src="" >
         </div>
 
 
