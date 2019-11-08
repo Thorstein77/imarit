3f030ba26888a828754494d46097179fa73b2f0b
Daniel <danielsimionescu1996@gmail.com>
2019-11-08 12:41:00 +0100

    Implement footer

--- wp-content/themes/imarit/header.php
+++ wp-content/themes/imarit/header.php
@@ -22,7 +22,7 @@
 	<?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
+<body style="margin-bottom: 0" <?php body_class(); ?>>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'imarit' ); ?></a>
 
